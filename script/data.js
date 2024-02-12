54a68fe22933d16ae9dc10ef51e64bb1165e6234
Daniel Harrison <d.harrison.lima@gmail.com>
2024-02-12 19:11:41 -0300

    Arquivo data que possui a lista de perguntas e alternativas

---
+++ script/data.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    question: "Qual jogador é conhecido como 'O Fenômeno'?",
+    options: ["Ronaldinho Gaúcho", "Lionel Messi", "Cristiano Ronaldo", "Ronaldo Nazário"],
+    answer: 3
+  },
+  {
+    question: "Qual clube ganhou mais títulos da Liga dos Campeões da UEFA até 2022?",
+    options: ["Real Madrid", "FC Barcelona", "Bayern de Munique", "Liverpool"],
+    answer: 0
+  },
+  {
+    question: "Quem é o maior artilheiro da história da Copa do Mundo?",
+    options: ["Pelé", "Lionel Messi", "Cristiano Ronaldo", "Miroslav Klose"],
+    answer: 3
+  },
+  {
+    question: "Qual país venceu a Copa do Mundo de 2018?",
+    options: ["Alemanha", "Brasil", "Argentina", "França"],
+    answer: 3
+  },
+  {
+    question: "Quem é o técnico da Seleção Brasileira de Futebol em 2022?",
+    options: ["Tite", "Felipão", "Dunga", "Mano Menezes"],
+    answer: 0
+  }
+];
